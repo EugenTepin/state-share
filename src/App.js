1c69294cc102e051ddd33af86e545cf853949c5c
Eugen Tepin <4godwork@gmail.com>
2020-10-06 01:06:45 +0300

    Service worker

--- src/App.js
+++ src/App.js
@@ -8,25 +8,48 @@ function getRandomInt(min, max) {
 }
 
 function pickColor() {
-  const colors = ['gold', 'royalblue', 'tomato', 'teal', 'darkgrey'];
+  const colors = ['gold', 'royalblue', 'tomato', 'teal', 'darkgrey', 'firebrick', 'mediumspringgreen', 'mediumslateblue', 'indigo', 'maroon'];
   return colors[getRandomInt(0, colors.length)];
 }
 
-function App(worker) {
+function App() {
   const [bgColor, setBgColor] = useState('tomato');
+  const [subscribed, setSubscription] = useState(true);
+
+  function dispatchState() {
+    const color = pickColor();
+    setBgColor(color);
+    navigator.serviceWorker.ready.then(registration => {
+      registration.active.postMessage({ color });
+    });
+  }
+
+  const handler = (event) => {
+    setBgColor(event.data.color);
+    console.log(`color: ${event.data.color}`);
+  };
+
+  function unsubscribe() {
+    navigator.serviceWorker.removeEventListener('message', handler);
+    setSubscription(false);
+  }
 
   useEffect(() => {
-    return () => {
-    };
+    if (subscribed) {
+      navigator.serviceWorker.addEventListener('message', handler);
+      return () => {
+        navigator.serviceWorker.removeEventListener('message', handler);
+      }
+    }
   });
 
   return (
     <div className="app grid-container" style={{ background: bgColor }}>
       <div className="A">
-        <button style={{ background: bgColor }} onClick={() => setBgColor(pickColor())}> Change State </button>
+        <button style={{ background: bgColor }} onClick={() => dispatchState()}> Change State </button>
       </div>
       <div className="B">
-        <button style={{ background: bgColor }} onClick={() => {}}> Unsubscribe </button>
+        <button style={{ background: bgColor }} onClick={() => unsubscribe()}> Unsubscribe </button>
       </div>
     </div>
   );
