60c61572f63f52ab5fc5bc08f9d3dd7d541ddadc
Eugen Tepin <4godwork@gmail.com>
2020-10-07 03:23:20 +0300

    BroadcastChannel

--- src/App.js
+++ src/App.js
@@ -8,25 +8,43 @@ function getRandomInt(min, max) {
 }
 
 function pickColor() {
-  const colors = ['gold', 'royalblue', 'tomato', 'teal', 'darkgrey'];
+  const colors = ['gold', 'royalblue', 'tomato', 'teal', 'darkgrey', 'firebrick', 'mediumspringgreen', 'mediumslateblue', 'indigo', 'maroon'];
   return colors[getRandomInt(0, colors.length)];
 }
 
-function App(worker) {
+function App() {
   const [bgColor, setBgColor] = useState('tomato');
+  const [subscribed, setSubscription] = useState(true);
+  const bc = new BroadcastChannel('shared_state');
+  function dispatchState() {
+    const color = pickColor();
+    setBgColor(color);
+    bc.postMessage({color});
+  }
+
+  function unsubscribe() {
+    setSubscription(false);
+    bc.close();
+  }
 
   useEffect(() => {
-    return () => {
-    };
+    if(subscribed){
+      bc.onmessage = ({data}) => {
+        setBgColor(data.color);
+      };
+      return () => {
+        bc.close();
+      };
+    }
   });
 
   return (
     <div className="app grid-container" style={{ background: bgColor }}>
       <div className="A">
-        <button style={{ background: bgColor }} onClick={() => setBgColor(pickColor())}> Change State </button>
+        <button style={{ background: bgColor }} onClick={() => dispatchState()}> Change State </button>
       </div>
       <div className="B">
-        <button style={{ background: bgColor }} onClick={() => {}}> Unsubscribe </button>
+        <button style={{ background: bgColor }} onClick={() => unsubscribe()}> Unsubscribe </button>
       </div>
     </div>
   );
